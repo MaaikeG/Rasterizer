047b7cf3fac4e7f0e73b5be72194d662924e957d
Maaike <maaikegalama@outlook.com>
2017-06-06 12:02:12 +0200

    rough implementation of scenegraph and merge

---
+++ SceneGraph.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template_P3;
+
+namespace template_P3
+{
+    class SceneGraph
+    {
+        List<Mesh> children = new List<Mesh>();
+
+        public void Render(Shader shader, Matrix4 transform)
+        {
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].Render(shader, transform);
+            }
+        }
+
+        internal void AddChild(Mesh mesh, Texture texture)
+        {
+            mesh.SetTexture(texture);
+            children.Add(mesh);
+        }
+    }
+}
--- game.cs
+++ game.cs
@@ -2,91 +2,93 @@
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+using template_P3;
 
 // minimal OpenTK rendering framework for UU/INFOGR
 // Jacco Bikker, 2016
 
-namespace Template_P3 {
-
-class Game
+namespace Template_P3
 {
-	// member variables
-	public Surface screen;					// background surface for printing etc.
-	Mesh mesh, floor;						// a mesh to draw using OpenGL
-	const float PI = 3.1415926535f;			// PI
-	float a = 0;							// teapot rotation angle
-	Stopwatch timer;						// timer for measuring frame duration
-	Shader shader;							// shader to use for rendering
-	Shader postproc;						// shader to use for post processing
-	Texture wood;							// texture to use for rendering
-	RenderTarget target;					// intermediate render target
-	ScreenQuad quad;						// screen filling quad for post processing
-	bool useRenderTarget = true;
 
-	// initialize
-	public void Init()
-	{
-		// load teapot
-		mesh = new Mesh( "../../assets/teapot.obj" );
-		floor = new Mesh( "../../assets/floor.obj" );
-		// initialize stopwatch
-		timer = new Stopwatch();
-		timer.Reset();
-		timer.Start();
-		// create shaders
-		shader = new Shader( "../../shaders/vs.glsl", "../../shaders/fs.glsl" );
-		postproc = new Shader( "../../shaders/vs_post.glsl", "../../shaders/fs_post.glsl" );
-		// load a texture
-		wood = new Texture( "../../assets/wood.jpg" );
-		// create the render target
-		target = new RenderTarget( screen.width, screen.height );
-		quad = new ScreenQuad();
-   	}
+    class Game
+    {
+        // member variables
+        public Surface screen;                  // background surface for printing etc.
+        public SceneGraph scene;
+        const float PI = 3.1415926535f;         // PI
+        float a = 0;                            // teapot rotation angle
+        Stopwatch timer;                        // timer for measuring frame duration
+        Shader shader;                          // shader to use for rendering
+        Shader postproc;                        // shader to use for post processing
+        RenderTarget target;                    // intermediate render target
+        ScreenQuad quad;                        // screen filling quad for post processing
+        bool useRenderTarget = true;
 
-	// tick for background surface
-	public void Tick()
-	{
-		screen.Clear( 0 );
-		screen.Print( "hello world", 2, 2, 0xffff00 );
-	}
+        // initialize
+        public void Init()
+        {
+            // create shaders
+            shader = new Shader("../../shaders/vs.glsl", "../../shaders/fs.glsl");
+            postproc = new Shader("../../shaders/vs_post.glsl", "../../shaders/fs_post.glsl");
+            // load a texture
+            Texture wood = new Texture("../../assets/wood.jpg");
+            // create the render target
 
-	// tick for OpenGL rendering code
-	public void RenderGL()
-	{
-		// measure frame duration
-		float frameDuration = timer.ElapsedMilliseconds;
-		timer.Reset();
-		timer.Start();
-	
-		// prepare matrix for vertex shader
-		Matrix4 transform = Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0 ), a );
-		transform *= Matrix4.CreateTranslation( 0, -4, -15 );
-		transform *= Matrix4.CreatePerspectiveFieldOfView( 1.2f, 1.3f, .1f, 1000 );
+            scene = new SceneGraph();
+            scene.AddChild(new Mesh("../../assets/teapot.obj"), wood);
+            scene.AddChild(new Mesh("../../assets/floor.obj"), wood);
 
-		// update rotation
-		a += 0.001f * frameDuration; 
-		if (a > 2 * PI) a -= 2 * PI;
+            // initialize stopwatch
+            timer = new Stopwatch();
+            timer.Reset();
+            timer.Start();
+            
+            target = new RenderTarget(screen.width, screen.height);
+            quad = new ScreenQuad();
+        }
 
-		if (useRenderTarget)
-		{
-			// enable render target
-			target.Bind();
+        // tick for background surface
+        public void Tick()
+        {
+            screen.Clear(0);
+            screen.Print("hello world", 2, 2, 0xffff00);
+        }
 
-			// render scene to render target
-			mesh.Render( shader, transform, wood );
-			floor.Render( shader, transform, wood );
+        // tick for OpenGL rendering code
+        public void RenderGL()
+        {
+            // measure frame duration
+            float frameDuration = timer.ElapsedMilliseconds;
+            timer.Reset();
+            timer.Start();
 
-			// render quad
-			target.Unbind();
-			quad.Render( postproc, target.GetTextureID() );
-		}
-		else
-		{
-			// render scene directly to the screen
-			mesh.Render( shader, transform, wood );
-			floor.Render( shader, transform, wood );
-		}
-	}
-}
+            // prepare matrix for vertex shader
+            Matrix4 transform = Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
+            transform *= Matrix4.CreateTranslation(0, -4, -15);
+            transform *= Matrix4.CreatePerspectiveFieldOfView(1.2f, 1.3f, .1f, 1000);
+
+            // update rotation
+            a += 0.001f * frameDuration;
+            if (a > 2 * PI) a -= 2 * PI;
+
+            if (useRenderTarget)
+            {
+                // enable render target
+                target.Bind();
+
+                // render the scene
+                scene.Render(shader, transform);
+
+                // render quad
+                target.Unbind();
+                quad.Render(postproc, target.GetTextureID());
+            }
+            else
+            {
+                // render scene directly to the screen
+                scene.Render(shader, transform);
+            }
+        }
+    }
 
 } // namespace Template_P3
