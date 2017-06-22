a971f72c797e1c53e9f6eac716d381c8f6d9e4ba
Maaike <maaikegalama@outlook.com>
2017-06-22 16:12:27 +0200

    Chromatic abberation

--- shaders/fs_post.glsl
+++ shaders/fs_post.glsl
@@ -9,16 +9,26 @@ uniform sampler3D colorCube;
 const float RADIUS = 0.55;
 const float SOFTNESS = 0.45;
 
+vec3 colorOffset = vec3 (0.05, 0.03, 0.01);
+
 // shader output
 out vec3 outputColor;
 
 void main()
 {
-	// retrieve input pixel
-	outputColor = texture( pixels, uv ).rgb;
-	// apply vignetting
+	// Calculate distance from screen centre
 	float dx = P.x - 0.5, dy = P.y - 0.5;
 	float distance = sqrt( dx * dx + dy * dy );
+	colorOffset *= distance;
+
+	// retrieve input pixel with chromatic abberation
+	outputColor = vec3 ( 
+      texture(pixels, vec2(uv.x + colorOffset.r, uv.y + colorOffset.r)).r, 
+      texture(pixels, vec2(uv.x + colorOffset.g, uv.y + colorOffset.g)).g,
+      texture(pixels, vec2(uv.x + colorOffset.b, uv.y + colorOffset.b)).b
+	  );
+
+	// apply vignetting
 	float vignette = smoothstep(RADIUS, SOFTNESS, distance);
 	outputColor = mix(outputColor.rgb, outputColor.rgb * vignette, 0.5);
 	
