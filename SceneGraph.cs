d43a593dbfea166399e39e4e767a05fdd67fe9aa
Maaike <maaikegalama@outlook.com>
2017-06-22 15:42:16 +0200

    Color cube works

--- SceneGraph.cs
+++ SceneGraph.cs
@@ -20,8 +20,6 @@ namespace template_P3
             Plane[] bb = new Plane[6];
 
             var mt = Matrix4.Invert(Matrix4.Transpose(transform));
-            //bb[0] = new Plane(cam.orientation, cam.position + new Vector3(0, 0, 0.1f));
-            //bb[1] = new Plane(cam.orientation, cam.position + new Vector3(0, 0, 1000));
             bb[0] = new Plane(Vector3.Normalize(Vector3.Transform(new Vector3(1, 0, 0), mt)), Vector3.Transform(new Vector3(-1, 0, 0), mt)); // left
             bb[1] = new Plane(Vector3.Normalize(Vector3.Transform(new Vector3(-1, 0, 0), mt)), Vector3.Transform(new Vector3(1, 0, 0), mt)); // right
             bb[2] = new Plane(Vector3.Normalize(Vector3.Transform(new Vector3(0, 1, 0), mt)), Vector3.Transform(new Vector3(0, -1, 0), mt));
--- shaders/fs_post.glsl
+++ shaders/fs_post.glsl
@@ -4,6 +4,7 @@
 in vec2 P;						// fragment position in screen space
 in vec2 uv;						// interpolated texture coordinates
 uniform sampler2D pixels;		// input texture (1st pass render target)
+uniform sampler3D colorCube;
 
 // shader output
 out vec3 outputColor;
@@ -12,10 +13,10 @@ void main()
 {
 	// retrieve input pixel
 	outputColor = texture( pixels, uv ).rgb;
-	// apply dummy postprocessing effect
-	float dx = P.x - 0.5, dy = P.y - 0.5;
-	float distance = sqrt( dx * dx + dy * dy );
-	outputColor *= sin( distance * 200.0f ) * 0.25f + 0.75f;
+	
+	const vec3 offset = vec3(1.0 / (2.0 * 16));
+	outputColor = texture3D(colorCube, vec3((16 - 1.0) / 16) * outputColor.xyz + offset).rgb;
 }
+
 
 // EOF
